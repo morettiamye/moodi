b5f750cd98e7e98b908d7badd2b86a21aed267c6
Amy <amy@purr-programming.com>
2022-03-13 13:09:00 -0500

    initial storyboards

---
+++ moodi/mood.swift
@@ -0,0 +1,17 @@
+//
+//  mood.swift
+//  moodi
+//
+//  Created by Amy Moretti on 3/13/22.
+//
+
+import Foundation
+
+struct todaysMood {
+    var mood = ""
+    var weather = ""
+    var sleep = ""
+    var notes = ""
+    var contributors = ""
+}
+
